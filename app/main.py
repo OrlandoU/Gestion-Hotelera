e842bd0ce9212216b582673d70dad7010391107d
Orlando Umanzor <71046838+OrlandoU@users.noreply.github.com>
2026-06-26 03:32:57 +0000

    Create connection

--- app/main.py
+++ app/main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
 from app.routes import reportes_router
+from os import getenv
+from dotenv import load_dotenv
+from mssql_python import connect
+
+load_dotenv()
+conn = connect(getenv("SQL_CONNECTION_STRING"))
 
 app = FastAPI(title="My Modular API")
 
--- app/routes/reportes.py
+++ app/routes/reportes.py
@@ -13,3 +13,40 @@ async def read_reportes():
 @router.get("/{item_id}")
 async def read_reporte(item_id: int):
     return {"item_id": item_id, "name": "Specific Item"}
+
+# REPORTES 
+@router.get("/clientes")
+async def read_clientes():
+    return {"Hello": "World"}
+
+@router.get("reservaciones-diarias")
+async def read_reservaciones_diarias():
+    return {"Hello": "World"}
+
+@router.get("estado-de-habitaciones")
+async def read_estado_de_habitaciones():
+    return {"Hello": "World"}
+
+@router.get("actividades-mantenimientos-diarias")
+async def read_actividades_mantenimientos_diarias():
+    return {"Hello": "World"}
+
+@router.get("pagos-realizados")
+async def read_pagos_realizados():
+    return {"Hello": "World"}
+
+@router.get("consumo-stock-semanal")
+async def read_consumo_stock_semanal():
+    return {"Hello": "World"}
+
+@router.get("estadistica-ocupacion-mensual")
+async def read_estadistica_ocupacion_mensual():
+    return {"Hello": "World"}
+
+@router.get("ingresos-tipo-habitacion")
+async def read_ingresos_tipo_habitacion():
+    return {"Hello": "World"}
+
+@router.get("consumo-amenidades-mensual")
+async def read_consumo_amenidades_mensual():
+    return {"Hello": "World"}
